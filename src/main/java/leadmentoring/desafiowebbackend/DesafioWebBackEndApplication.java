46dc3116f1bea3e17ca0ce68d55cceca0fe17e4f
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-11-25 12:08:31 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/leadmentoring/desafiowebbackend/DesafioWebBackEndApplication.java
@@ -0,0 +1,13 @@
+package leadmentoring.desafiowebbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioWebBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioWebBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/leadmentoring/desafiowebbackend/DesafioWebBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package leadmentoring.desafiowebbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioWebBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
