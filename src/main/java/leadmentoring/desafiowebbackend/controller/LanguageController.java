8ae0aef5cf1db5cfe30d85c24f7d17a8a777b878
Gabriel Barros <gabrielbarros201612@gmail.com>
2021-11-29 12:10:10 -0300

    Criação de 3 endpoints para usuários e idiomas (FindById, Save e List all)

---
+++ src/main/java/leadmentoring/desafiowebbackend/controller/LanguageController.java
@@ -0,0 +1,43 @@
+package leadmentoring.desafiowebbackend.controller;
+
+import leadmentoring.desafiowebbackend.domain.Language;
+import leadmentoring.desafiowebbackend.domain.Users;
+import leadmentoring.desafiowebbackend.dtos.LanguageDTO;
+import leadmentoring.desafiowebbackend.dtos.UsersDTO;
+import leadmentoring.desafiowebbackend.service.LanguageService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.time.LocalDateTime;
+import java.util.List;
+
+@RestController
+@RequestMapping("language")
+@RequiredArgsConstructor
+@Log4j2
+public class LanguageController {
+
+    private final LanguageService languageService;
+
+    @GetMapping
+    public List<Language> listAllNoPageable(){
+        return languageService.listAllNoPageable();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Language> findById(@PathVariable long id){
+        return new ResponseEntity(languageService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Language> save(@RequestBody @Valid LanguageDTO languageDTO){
+        Language language = languageService.save(languageDTO);
+        log.info(language);
+        return new ResponseEntity(language, HttpStatus.CREATED);
+    }
+
+}
---
+++ src/main/java/leadmentoring/desafiowebbackend/controller/UserController.java
@@ -0,0 +1,40 @@
+package leadmentoring.desafiowebbackend.controller;
+
+import leadmentoring.desafiowebbackend.domain.Language;
+import leadmentoring.desafiowebbackend.domain.Users;
+import leadmentoring.desafiowebbackend.dtos.UsersDTO;
+import leadmentoring.desafiowebbackend.service.UserService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+
+@RestController
+@RequestMapping("users")
+@RequiredArgsConstructor
+@Log4j2
+public class UserController {
+
+    private final UserService userService;
+
+    @GetMapping
+    public List<Users> listAllNoPageable(){
+        return userService.listAllNoPageable();
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<Language> findById(@PathVariable long id){
+        return new ResponseEntity(userService.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Users> save(@RequestBody @Valid UsersDTO usersDTO){
+        Users user = userService.save(usersDTO);
+        log.info(user);
+        return new ResponseEntity(user, HttpStatus.CREATED);
+    }
+}
