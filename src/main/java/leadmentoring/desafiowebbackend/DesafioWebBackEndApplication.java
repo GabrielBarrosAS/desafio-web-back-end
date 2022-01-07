c28835f850de31536daa4a6875718f663b705e30
Gabriel Barros <gabrielbarros201612@gmail.com>
2022-01-07 13:11:58 -0300

    Iniciando autenticação sem conexão com o servidor

--- src/main/java/leadmentoring/desafiowebbackend/DesafioWebBackEndApplication.java
+++ src/main/java/leadmentoring/desafiowebbackend/DesafioWebBackEndApplication.java
@@ -8,6 +8,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 @SecurityScheme(name = "Movie System", scheme = "basic", type = SecuritySchemeType.HTTP, in = SecuritySchemeIn.HEADER)
@@ -21,5 +23,15 @@ public class DesafioWebBackEndApplication {
 	public PasswordEncoder getPasswordEncoder(){
 		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 		return encoder;
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 }
